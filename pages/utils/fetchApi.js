0047b92912e92f2776ce954d889086c17da8ef9e
Olga Zinchuk <olga.zinchuk@hotmail.com>
2022-08-04 14:01:57 -0400

    Project setup

---
+++ pages/utils/fetchApi.js
@@ -0,0 +1,13 @@
+// import axios from 'axios';
+
+// export const baseUrl = 'http://bayut.p.rapidapi.com'
+
+// export const fetchApi = async (url) => {
+//     const { data } = await axios.get((url), {
+//         headers: {
+//             'X-RapidAPI-Host': 'bayut.p.rapidapi.com',
+//             'X-RapidAPI-Key': 'bfb198e916msh1b83bf7ea1955ebp13f64djsn45144dbdb320'
+//           }
+//     } );
+//     return data;
+// }
